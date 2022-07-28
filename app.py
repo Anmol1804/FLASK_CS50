26c9303ab9ee580f5fbe1641c041a4253a43ce6b
Anmol1804 <anmolgera01@gmail.com>
2022-07-28 23:34:10 +0530

    do

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+
+@app.route("/greet", methods=["post"])
+def greet():
+    name = request.form.get('name', 'world')
+    return render_template('greet.html', name=name)
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+# import sys
+# print(sys.path)
---
+++ froshmis/app.py
@@ -0,0 +1,44 @@
+import re
+from flask import Flask, redirect, render_template, request
+
+app = Flask(__name__)
+
+REGISTRANTS = {}
+
+SPORTS = [
+    "Basketball",
+    "Soccer",
+    "Frisbee"
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html', sports=SPORTS)
+
+
+@app.route('/register', methods=['post'])
+def register():
+
+    # validate name
+    name = request.form.get('name')
+    if not name:
+        return render_template('error.html', message="Missing name")
+
+    # validate submission
+    sport = request.form.get('sport')
+    if not sport:
+        return render_template('error.html', message="Missing sport")
+    
+    if sport not in SPORTS:
+        return render_template('error.html', message="Invalid sport")
+
+    # Remember REGISTRANTS
+    REGISTRANTS[name] = sport
+
+    # confirm registration
+    return redirect("/registration")    
+
+
+@app.route('/registration')
+def registeration():
+    return render_template('registration.html', registrants = REGISTRANTS)
